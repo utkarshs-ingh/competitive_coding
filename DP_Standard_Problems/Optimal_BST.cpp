2ae38460e2a50ca72f826155edc104e73f211787
Utkarsh singh <utkarshsingh.1306@gmail.com>
2020-08-23 08:59:31 +0530

    Update Optimal_BST.cpp

--- DP_Standard_Problems/Optimal_BST.cpp
+++ DP_Standard_Problems/Optimal_BST.cpp
@@ -37,6 +37,37 @@ int OBT(vector<ll>freq, ll i, ll j){ //Recursion no DP: OPTIMAL BINARY SEARCH TR
     return ans + sum;
 }
 
+int OBT(vector<ll>key, vector<ll>freq, ll n){ //ITERATIVE bottom up Dp: OPTIMAL BINARY SEARCH TREE
+
+    ll dp[n][n];
+    memset(dp, 0, sizeof(dp));
+
+    FOR(i, 0, n){
+       dp[i][i] = freq[i];
+    }
+
+    FOR(l, 2, n+1){
+        FOR(i, 0, n-l+1){
+            ll j = l + i - 1;
+            dp[i][j] = INT_MAX;
+            ll sum = freqsum(freq, i, j);
+
+            FOR(r, i, j+1){
+                ll cost = sum + (r - 1 < i ? 0 : dp[i][r-1]) + (r + 1 > j ? 0 : dp[r+1][j]);
+                dp[i][j] = min(cost, dp[i][j]);
+            }
+        }
+    }
+
+    // FOR(i, 0, n){
+    //     FOR(j, 0, n)
+    //         cout << dp[i][j] << " ";
+    //     nl
+    // }
+    cout << dp[0][n-1]; 
+
+}
+
 void solve() {
 	ll n, a;
     cin >> n;
@@ -48,7 +79,7 @@ void solve() {
     FOR(i, 0, n) cin >> a, freq.push_back(a);
 
     cout << OBT(freq, 0, n-1); nl
-    // OBT(key, freq, n); nl
+    OBT(key, freq, n); nl
 }
 
 /*----------------------------------------------------------------------------------------------------*/
